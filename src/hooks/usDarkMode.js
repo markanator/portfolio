debd42f8ea95d7435c9db9fbac291737cb26b2e3
Mark Ambro <wowmarkambro@gmail.com>
2020-10-30 18:01:03 -0500

    🐛 removed localstorage

--- src/hooks/usDarkMode.js
+++ src/hooks/usDarkMode.js
@@ -1,18 +1,17 @@
-import { useEffect } from 'react';
-import { useLocalStorage } from './useLocalStorage';
+import { useEffect, useState } from 'react'
 
 export const useDarkMode = (key, initialValues) => {
-  const [values, setValues] = useLocalStorage(key, initialValues)
+  const [values, setValues] = useState(false)
 
   useEffect(() => {
-    const body = document.getElementsByTagName('body')[0];
+    const body = document.getElementsByTagName('body')[0]
 
     if (values) {
       // add
-      body.classList.add('dark');
+      body.classList.add('dark')
     } else {
       // remove class from body element
-      body.classList.remove('dark');
+      body.classList.remove('dark')
     }
   }, [values])
 
