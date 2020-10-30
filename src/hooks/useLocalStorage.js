6848c3f2b9e58ed7cc32d108c612e3a6b03da658
Mark Ambro <wowmarkambro@gmail.com>
2020-10-30 17:14:57 -0500

    🚀 hot fix for localstorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export const useLocalStorage = (key, initialValue) => {
+  // console.log(module)
   // set up state property
   const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
+    const item =
+      typeof window !== 'undefined' && window.localStorage.getItem(key)
     // if localstorage data use that, itherwuse use initvals
-    return item ? JSON.parse(item) : initialValue;
-  });
+    return item ? JSON.parse(item) : initialValue
+  })
 
   const setValue = value => {
-    setStoredValue(value);
+    setStoredValue(value)
     // update localstorage
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
 
-  return [storedValue, setValue];
-};
+  return [storedValue, setValue]
+}
